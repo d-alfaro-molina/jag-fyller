89da8cb80baa19e1892c4beda7bac9cb683dbe55
dalfa <dalfa@LAPTOP-5OVDE9DL>
2021-08-24 15:30:29 +0200

    Add project files.

---
+++ jag fyller/UnitTest1.cs
@@ -0,0 +1,48 @@
+using System;
+using System.ComponentModel.Design;
+using Xunit;
+using Xunit.Sdk;
+
+namespace jag_fyller
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(5, 2 + 3);
+
+        }
+
+        [Fact]
+
+        public void Test1()
+        {
+            Assert.True("jag_fyller jag_fyller" + 29 == "Jag har fyllt 29");
+        }
+
+        
+
+        {
+
+            int bignumber = 2147483647;
+            int biggerNUmber = bignumber + 1;
+            Assert.True(biggerNUmber > bignumber);
+        }
+
+        private string purchasedDrink;
+        private int age = 28;
+        
+        if (age > 18)
+
+        {
+            purchasedDrink = "Beer";
+
+        }
+        IExtenderListService
+
+
+
+
+    }
+}
